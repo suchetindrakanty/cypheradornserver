5303752687907978108dba69ea542a5c9d334a81
suchetindrakanty <mi0813@srmist.edu.in>
2024-04-20 17:37:00 +0530

    first commit

--- Server/app.py
+++ Server/app.py
@@ -619,5 +619,6 @@ def updateOrderStatus():
         'message': 'Success'
     }
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True)
+@app.route('/')
+def asd():
+    return 'asd'
